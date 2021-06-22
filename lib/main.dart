922d7cf433ae0127ef07ab5f78545ce21b2dd0f2
makhosravi <peter_stark95@yahoo.com>
2021-06-22 12:31:08 +0430

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CustomDrawer(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class CustomDrawer extends StatefulWidget {
+  CustomDrawer({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _CustomDrawerState createState() => _CustomDrawerState();
+}
+
+class _CustomDrawerState extends State<CustomDrawer> with SingleTickerProviderStateMixin {
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.white,);
+  }
+}
