b355d8d867e9f541abe11c706ba02f2022d26170
makhosravi <peter_stark95@yahoo.com>
2021-06-24 11:59:02 +0430

    moved drawer to drawer.dart

--- lib/custom_drawer.dart
+++ lib/custom_drawer.dart
@@ -1,3 +1,4 @@
+import 'package:complex_ui1/drawer.dart';
 import 'package:flutter/material.dart';
 
 class CustomDrawer extends StatefulWidget {
@@ -111,58 +112,6 @@ class _CustomDrawerState extends State<CustomDrawer> with SingleTickerProviderSt
               ],
             );
           },
-        ),
-      ),
-    );
-  }
-}
-
-class MyDrawer extends StatelessWidget {
-  const MyDrawer({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: Colors.blueAccent,
-      child: SafeArea(
-        child: Theme(
-          data: ThemeData(
-            brightness: Brightness.dark,
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            children: [
-              UserAccountsDrawerHeader(
-                accountName: Text('MAKH'),
-                accountEmail: Text('makh@sth.com'),
-                currentAccountPicture: FlutterLogo(),
-                decoration: BoxDecoration(
-                  color: Colors.blueAccent,
-                ),
-              ),
-              ListTile(
-                leading: Icon(Icons.new_releases),
-                title: Text('News'),
-              ),
-              ListTile(
-                leading: Icon(Icons.star),
-                title: Text('Favorites'),
-              ),
-              ListTile(
-                leading: Icon(Icons.map),
-                title: Text('Map'),
-              ),
-              ListTile(
-                leading: Icon(Icons.settings),
-                title: Text('Settings'),
-              ),
-              ListTile(
-                leading: Icon(Icons.person),
-                title: Text('Profile'),
-              ),
-            ],
-          ),
         ),
       ),
     );
---
+++ lib/drawer.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.blueAccent,
+      child: SafeArea(
+        child: Theme(
+          data: ThemeData(
+            brightness: Brightness.dark,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              UserAccountsDrawerHeader(
+                accountName: Text('MAKH'),
+                accountEmail: Text('makh@sth.com'),
+                currentAccountPicture: FlutterLogo(),
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.new_releases),
+                title: Text('News'),
+              ),
+              ListTile(
+                leading: Icon(Icons.star),
+                title: Text('Favorites'),
+              ),
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Map'),
+              ),
+              ListTile(
+                leading: Icon(Icons.settings),
+                title: Text('Settings'),
+              ),
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Profile'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
